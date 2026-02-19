15b947293e913386d552e0ce81e45932f965c3ff
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-19 16:36:52 +0100

    route to the hero detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AllHeroes} from './components/all-heroes/all-heroes';
+import {HeroDetail} from './components/hero-detail/hero-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path : '', component : AllHeroes },
+  { path : '/hero', component : HeroDetail }
+];
