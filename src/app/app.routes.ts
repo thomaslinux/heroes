acd74bfa1b8089e8898f540d2430e9421938df18
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-19 16:44:31 +0100

    utilize router-outlet in app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {AllHeroes} from './components/all-heroes/all-heroes';
 import {HeroDetail} from './components/hero-detail/hero-detail';
 
 export const routes: Routes = [
-  { path : '', component : AllHeroes },
-  { path : '/hero', component : HeroDetail }
+  {path: "", component: AllHeroes},
+  {path: "hero", component: HeroDetail},
+  {path: "**", component: AllHeroes}
 ];
