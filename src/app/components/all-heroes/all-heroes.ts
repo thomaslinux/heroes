9166ce9af58228d490c3530b9da2d14764d9d201
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-20 14:05:00 +0100

    no api

--- src/app/components/all-heroes/all-heroes.ts
+++ src/app/components/all-heroes/all-heroes.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {JsonPipe} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {Hero} from '../../models/hero';
 
 @Component({
   selector: 'app-all-heroes',
@@ -12,25 +13,15 @@ import {RouterLink} from '@angular/router';
   styleUrl: './all-heroes.css',
 })
 export class AllHeroes {
-  public heroes : any[];
+  public heroes : Hero[];
 
   constructor() {
     this.heroes = [];
   }
 
   public async addHeroes() {
-    // this.heroes.push({id : 1, name : 'Hulk', image : 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/332-hulk.jpg'});
-    // this.heroes.push({id : 2, name : 'Batman', image : 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/71-batman-ii.jpg'});
-    // this.heroes.push({id : 3, name : 'Buffy', image : 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/140-buffy.jpg'});
-    this.heroes = await callApi("https://akabab.github.io/superhero-api/api/all.json");
-    console.log(this.heroes);
-  }
-}
-
-async function callApi(url: string) {
-  let response = await fetch(url);
-
-  if (response.ok) {
-    return response.json();
+    this.heroes.push({id : 1, name : 'Hulk', image : 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/332-hulk.jpg'});
+    this.heroes.push({id : 2, name : 'Batman', image : 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/71-batman-ii.jpg'});
+    this.heroes.push({id : 3, name : 'Buffy', image : 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/140-buffy.jpg'});
   }
 }
